4177d62ada5054d447a45e1eb92f947bbcc3ed1f
MadinaMukhiddinovna <madinamukhiddinovnaa@gmail.com>
2025-03-12 01:44:44 +0500

    Update lesson5.sql

--- lesson-5/homework/lesson5.sql
+++ lesson-5/homework/lesson5.sql
@@ -1,1 +1,7 @@
+----first option
+SELECT ProductName as Name FROM Products;
+SELECT * FROM Customers as Client;
 
+SELECT ProductName FROM Products  
+UNION  
+SELECT ProductName FROM Products_Discontinued;
